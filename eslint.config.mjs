27b9f401f23df83718a3c595f7b8167d225cf95d
Henri Fournier <hfournier@intratela.com>
2026-09-03 09:45:35 -0700

    added linting

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+// @ts-check
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import astro from 'eslint-plugin-astro';
+import prettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+export default tseslint.config(
+  {
+    ignores: ['dist/', '.astro/', 'node_modules/', 'public/'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...astro.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  prettier
+);
